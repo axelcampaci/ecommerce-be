908a797ac771ff792719955d4a9119baae69c554
axelcampaci <73192691+axelcampaci@users.noreply.github.com>
2023-12-01 15:37:04 +0100

    Implemented max total price record getter function

--- src/App.js
+++ src/App.js
@@ -7,15 +7,31 @@ function loadOrderList() {
   try {
     const file = fs.readFileSync(argv[2], "utf8");
     list = file.split("\r\n").slice(1);
-    console.log(list);
+    // console.log(list);
   } catch (err) {
     console.error(err);
   }
 }
 
+function getMaxTotalPriceRecord() {
+  let max = 0;
+  let maxRecord = "None";
+  list.forEach((record) => {
+    const recordArray = record.split(",");
+    const value = recordArray[2] * recordArray[3];
+    if (value >= max) {
+      max = value;
+      maxRecord = record;
+    }
+  });
+  return maxRecord;
+}
+
 function main() {
   loadOrderList();
-  //   console.log(getMaxTotalPriceRecord());
+  console.log(
+    "The record with the highest total price is: " + getMaxTotalPriceRecord()
+  );
   //   console.log(getMaxQuantityRecord());
   //   console.log(getMaxTotalDiscountRecord());
 }
